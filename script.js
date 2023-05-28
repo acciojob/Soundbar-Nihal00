d1f65538e822c93756511e6766df1ea57da14842
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-28 14:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 //your JS code here. If required.
-var audio = document.getElementById("audio");
-var buttons = document.getElementById("buttons");
+let audio = document.getElementById("audio");
+let buttons = document.querySelectorAll('button');
+let audio0 = new Audio("https://github.com/acciojob/Soundbar-Nihal00/blob/master/sounds/applause.mp3");
+let audio1 = new Audio("https://github.com/acciojob/Soundbar-Nihal00/blob/master/sounds/boo.mp3");
+let audio2 = new Audio("https://github.com/acciojob/Soundbar-Nihal00/blob/master/sounds/gasp.mp3");
+let audio3 = new Audio("https://github.com/acciojob/Soundbar-Nihal00/blob/master/sounds/tada.mp3");
+let audio4 = new Audio("https://github.com/acciojob/Soundbar-Nihal00/blob/master/sounds/victory.mp3");
+let audio5 = new Audio("https://github.com/acciojob/Soundbar-Nihal00/blob/master/sounds/wrong.mp3");
 
-buttons.addEventListener("click", function(event) {
-  var button = event.target;
-  if (button.className === "btn") {
-    audio.play();
-  } else if (button.className === "stop") {
-    audio.pause();
-  }
-});
+for(let i = 0; i < buttons.length; i++){
+	buttons[i].addEventListener("click", function(event) {
+	  var button = event.target;
+	  if (button.className === "btn") {
+	    audio+[i].play();
+	  } else if (button.className === "stop") {
+	    audio.pause();
+	  }
+	});
+}
+
+
+
