6fbf0c05ca54c3fbd87637bbae7be403da75a7f6
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-28 14:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var audio = document.getElementById("audio");
+var buttons = document.getElementById("buttons");
+
+buttons.addEventListener("click", function(event) {
+  var button = event.target;
+  if (button.className === "btn") {
+    audio.play();
+  } else if (button.className === "stop") {
+    audio.pause();
+  }
+});
